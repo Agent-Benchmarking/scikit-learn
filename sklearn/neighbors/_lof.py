84fb7495790eb2e32f87b83d62e6f3e34b834fc8
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-03-21 09:46:08 +0000

    DOC Add example links to NearestNeighbors and LocalOutlierFactor docstrings

--- sklearn/neighbors/_lof.py
+++ sklearn/neighbors/_lof.py
@@ -179,7 +179,10 @@ class LocalOutlierFactor(KNeighborsMixin, OutlierMixin, NeighborsBase):
     >>> clf.fit_predict(X)
     array([ 1,  1, -1,  1])
     >>> clf.negative_outlier_factor_
-    array([ -0.9821...,  -1.0370..., -73.3697...,  -0.9821...])
+    array([-0.9821..., -1.0370..., -73.3697..., -0.9821...])
+
+    For an example of outlier detection using LocalOutlierFactor, see
+    :ref:`sphx_glr_auto_examples_neighbors_plot_lof_outlier_detection.py`.
     """
 
     _parameter_constraints: dict = {
